e3a40d9fe9b7d8d10adc8268d3243a3e33c89a1d
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-06-01 01:37:34 +0200

    improved the scripts to includes big pokemons
    not yet fully integrated

--- utils/sprite_sheet_utils.js
+++ utils/sprite_sheet_utils.js
@@ -20,27 +20,80 @@ let y = 0;
 let spritesheet_width = 1024;
 let spritesheet_height = 5408;
 
-const pokemonWithGendersIds = [3, 25, 154, 202, 208, 214, 415, 443, 444, 445, 449, 450];
+const skips = [
+    {
+        from: {
+            x: 832,
+            y: 1536
+        }, to: {
+            x: 512,
+            y: 1760
+        }
+    }
+]
+
+// it needs to be fixed in the future
+// 487 is Giratina, he does not have two genders but has 2 forms
+const pokemonWithGendersIds = [3, 25, 154, 202, 208, 214, 415, 443, 444, 445, 449, 450, 487]; // <-- this is a hack
+const bigPokemons = ["Steelix", "Lugia", "Ho-oh", "Wailord", "Groudon", "Kyogre", "Rayquaza", "Dialga", "Palkia", "Regigigas", "Giratina", "Arceus"];
+let bigPokemonInRow = false;
 
 fs.readFileSync(filePath, 'utf8').split('\n').forEach((line) => {
-    // 001	Bulbasaur	Grass	Poison	45	49	49	65	65	45
     const [id, name, type1, type2, hp, attack, defense, spAttack, spDefense, speed] = line.split('\t');
-
     const pokemonData = { id, name, type1, type2, hp, attack, defense, spAttack, spDefense, speed, gender: 1 };
+    const isBigPokemon = bigPokemons.includes(pokemonData.name);
 
     let pokemon = formatPokemon(pokemonData, x, y);
+
+    if (isBigPokemon) {
+
+        if (x + width * 2 >= spritesheet_width) {
+            x = 0;
+            y += height * (bigPokemonInRow ? 6 : 4);
+        }
+
+        bigPokemonInRow = true;
+        pokemon.width = width * 2;
+        pokemon.height = height * 2;
+
+        console.log(`Big pokemon: ${pokemon.name}(${pokemon.id}) at ${x}, ${y}`);
+    }
+
     pokemons.push(pokemon);
 
-    if (pokemonWithGendersIds.includes(pokemon.id)) {
-        x += width * 2;
+    if (pokemonWithGendersIds.includes(parseInt(pokemon.id))) {
+        x += width * (isBigPokemon ? 4 : 2);
         pokemon = formatPokemon(pokemonData, x, y);
+
+        if (isBigPokemon) {
+            console.log(`Big pokemon (variant): ${pokemon.name}(${pokemon.id}) at ${x}, ${y}`)
+            pokemon.width = width * 2;
+            pokemon.height = height * 2;
+        }
+
         pokemons.push({ ...pokemon, gender: 2 });
     }
 
-    x += width * 2;
+    x += width * (isBigPokemon ? 4 : 2);
+
     if (x >= spritesheet_width) {
         x = 0;
-        y += height;
+        y += height * (bigPokemonInRow ? 6 : 4);
+
+        bigPokemonInRow = false;
+    }
+
+    if (pokemon.name === "Unown") {
+        x = 512;
+        y = 1760;
+
+        console.log(`Skipped ${pokemon.name} variants at ${x}, ${y}`);
+    }
+
+    if (pokemon.name === "Deoxys") {
+        console.log(`Skipped ${pokemon.name} variants at ${x}, ${y}`);
+        x = width * 2;
+        y += height * (bigPokemonInRow ? 6 : 4);
     }
 });
 
@@ -51,7 +104,7 @@ const json = {
     pokemons
 }
 
-fs.writeFileSync('src/assets/animations/pokemons/bw_old_overworld.json', JSON.stringify(json, null, 2));
+fs.writeFileSync('src/assets/json/bw_old_overworld.json', JSON.stringify(json, null, 2));
 
 function formatPokemon(pokemon, x, y) {
     return {
